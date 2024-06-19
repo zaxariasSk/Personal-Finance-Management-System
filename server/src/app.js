e0240e1bd107394e78f2fa3692928b595b555046
zaxariasSk <zack7abc7@gmail.com>
2024-06-20 00:25:05 +0300

    Project initialization and sequalize initialization

---
+++ server/src/app.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+const sequelize = require('./config/database');
+
+app.use(express.json());
+
+app.listen(process.env.PORT || 3000, async () => {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+
+    console.log('server is up and running in port: ' + process.env.PORT);
+});
---
+++ server/src/model/userModel.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+        allowNull: false
+    },
+    userName: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = User;
